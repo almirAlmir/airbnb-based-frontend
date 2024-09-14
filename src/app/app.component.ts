715e96c76e97b226d6d5f010a6c3bb230b8efe73
almirAlmir <aasc@cin.ufpe.br>
2024-09-14 14:39:40 -0300

    PrimeNg Button Hello World

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from "primeng/button"
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template:"<h1>Hello World</h1>",
+  imports: [ButtonModule],
+  templateUrl:"./app.component.html",
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
