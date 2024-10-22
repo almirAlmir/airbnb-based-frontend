6681dfeb7cf94a7df274b90489b1efa0b39a479d
almirAlmir <aasc@cin.ufpe.br>
2024-10-22 13:43:00 -0300

    toast service implementing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ButtonModule} from "primeng/button";
-import {FaIconLibrary, FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { ButtonModule } from "primeng/button";
+import { FaIconLibrary, FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
 import { FooterComponent } from "./layout/footer/footer.component";
+import { ToastModule } from "primeng/toast";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonModule, RouterOutlet, FontAwesomeModule, FooterComponent],
+  imports: [ButtonModule, RouterOutlet, FontAwesomeModule, FooterComponent, ToastModule],
   templateUrl:"./app.component.html",
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
 
   faIconLibrary = inject(FaIconLibrary);
+  isListingView = true;
 
   ngOnInit(): void {
       this.initFontAwesome();
